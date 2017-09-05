3577b17e10c5d475fb72c2b5c007385378ca3280
zhuangzz <1206838830@qq.com>
2017-09-05 09:53:09 +0800

    changecolor list

--- event-demo/src/components/App.js
+++ event-demo/src/components/App.js
@@ -3,7 +3,18 @@ import "./app.css"
 
 class App extends Component{
   state={
-    count:0
+    count:0,
+    comments: [
+      {
+        name: 'peter',
+        text: '你好'
+      },
+      {
+        name: 'billie',
+        text: 'Hi'
+      }
+    ],
+    checked: false
   }
   Sayhello =()=>{
     console.log("haha hello")
@@ -21,7 +32,16 @@ class App extends Component{
       count:this.state.count-1
     })
   }
+  ChangeColor=()=>{
+    this.setState({
+       checked: !this.state.checked
+    })
+  }
   render(){
+    const list = this.state.comments.map((item, i) => (
+      <li key={i}>{`${item.name}: ${item.text}`}</li>
+    ))
+    const style={backgroundColor: this.state.checked ? 'yellow' : 'blue'}
     return(<div className="app">
       <button onClick={this.Sayhello} className="cli">点击</button>
       <h1 onClick={this.Oneclick}>标题</h1>
@@ -29,6 +49,10 @@ class App extends Component{
       <h1>{this.state.count}</h1>
       <button onClick={this.ChangeMid}>-</button>
       <button onClick={this.ChangeCount}>+</button>
+      <ul className='app1'>
+         {list}
+      </ul>
+      <div className="color" onClick={this.ChangeColor} style={style}>123</div>
     </div>)
   }
 }
