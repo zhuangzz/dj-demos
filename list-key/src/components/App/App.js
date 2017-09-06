777ad1306f38c7b2ceb812314b5d83f468cf5907
zhuangzz <1206838830@qq.com>
2017-09-06 11:15:35 +0800

    choose-size

--- list-key/src/components/App/App.js
+++ list-key/src/components/App/App.js
@@ -1,15 +1,30 @@
 import React ,{Component} from "react"
 import "./app.css"
 class App extends Component{
-  handleClick=(say)=>{
-    console.log(`min ${say}`)
+  state = {
+  size : [
+    {size:"M"} ,
+    {size:"L"}  ,
+    {size:"XL"}  ,
+    {size:"XXL"}],
+  activeIndex:0
   }
-  render(){
-    let say="hello"
-    return(
-      <div className="app" onClick={()=>this.handleClick(say)}>
-        click
-      </div>
+  handleChose=(i)=>{
+    this.setState({
+      activeIndex:i
+    })
+    console.log(this.state.activeIndex)
+  }
+
+  render () {
+    const list=this.state.size.map((item,i) => (
+      <li key={i} onClick={()=>this.handleChose(i)} className={`lis ${i===this.state.activeIndex && "active"}`}
+         >{`${item.size}`}</li>
+    ))
+    return (
+      <ul className='app' >
+        {list}
+        </ul>
     )
   }
 }
